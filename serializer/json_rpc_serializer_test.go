db60b76f72d3d826b378bf0ccbfde85bd68f10ba
李铭昕 <715557344@qq.com>
2022-03-24 20:35:28 +0800

    Added test cases for Serialize with T

--- serializer/json_rpc_serializer_test.go
+++ serializer/json_rpc_serializer_test.go
@@ -28,3 +28,33 @@ func Test_Serializer_Serialize_And_UnSerialize(t *testing.T) {
 		c.So(data2.Context, c.ShouldEqual, data.Context)
 	})
 }
+
+func Test_Serializer_Serialize_And_UnSerialize_For_T(t *testing.T) {
+	c.Convey("Pack and UnPack must be interchangeable.", t, func() {
+		serializer := &JsonRPCSerializer{}
+		type DataFoo struct {
+			Id   int    `json:"id"`
+			Name string `json:"name"`
+		}
+
+		data := &JsonRPCData[*DataFoo, string]{
+			Id:      "123",
+			Path:    "/json_rpc/index",
+			Data:    &DataFoo{1, "Hyperf"},
+			Context: "Hello World",
+		}
+
+		ret, _ := serializer.Serialize(data)
+		json := "{\"id\":\"123\",\"path\":\"/json_rpc/index\",\"data\":{\"id\":1,\"name\":\"Hyperf\"},\"context\":\"Hello World\"}"
+		c.So(ret, c.ShouldEqual, json)
+
+		data2 := &JsonRPCData[*DataFoo, string]{}
+		serializer.UnSerialize(json, data2)
+
+		c.So(data2.Id, c.ShouldEqual, data.Id)
+		c.So(data2.Path, c.ShouldEqual, data.Path)
+		c.So(data2.Data.Id, c.ShouldEqual, data.Data.Id)
+		c.So(data2.Data.Name, c.ShouldEqual, data.Data.Name)
+		c.So(data2.Context, c.ShouldEqual, data.Context)
+	})
+}
