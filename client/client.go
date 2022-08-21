9826dd48e63af7dfda088f65c08b00067f6715b0
李铭昕 <715557344@qq.com>
2022-08-21 13:52:43 +0800

    Added Client::Send `[]byte`

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"github.com/hyperf/roc"
 	"github.com/hyperf/roc/serializer"
+	"net"
 )
 
 type Client struct {
@@ -11,18 +12,33 @@ type Client struct {
 	Serializer     serializer.SerializerInterface
 	PushChan       chan string
 	ChannelManager *roc.ChannelManager
+	Socket         net.Conn
 }
 
-func NewClient() *Client {
+func NewClient(conn net.Conn) *Client {
 	return &Client{
 		Packer:         &roc.Packer{},
 		IdGenerator:    &roc.IdGenerator{},
 		Serializer:     &serializer.JsonSerializer{},
 		PushChan:       make(chan string, 65535),
 		ChannelManager: roc.NewChannelManager(),
+		Socket:         conn,
 	}
 }
 
-func (c *Client) Send() {
+func (c *Client) Send(bt []byte) (uint32, error) {
+	id := c.IdGenerator.Generate()
 
+	_, err := c.Socket.Write(bt)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (c *Client) SendPacket(p *roc.Packet) (uint32, error) {
+	bt := c.Packer.Pack(p)
+
+	return c.Send(bt)
 }
--- client/client_test.go
+++ client/client_test.go
@@ -1,20 +1,86 @@
 package client
 
 import (
-	"fmt"
 	"github.com/hyperf/roc"
 	c "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/mock"
+	"net"
 	"testing"
+	"time"
 )
+
+type ConnMock struct {
+	mock.Mock
+}
+
+type AddrMock struct {
+}
+
+func (a AddrMock) Network() string {
+	return ""
+}
+
+func (a AddrMock) String() string {
+	return ""
+
+}
+
+func (c *ConnMock) Read(b []byte) (n int, err error) {
+	args := c.Called(b)
+	return args.Int(0), args.Error(1)
+}
+
+func (c *ConnMock) Write(b []byte) (n int, err error) {
+	args := c.Called(b)
+	return args.Int(0), args.Error(1)
+}
+
+func (c *ConnMock) Close() error {
+	return nil
+}
+
+func (c *ConnMock) LocalAddr() net.Addr {
+	return &AddrMock{}
+}
+
+func (c *ConnMock) RemoteAddr() net.Addr {
+	return &AddrMock{}
+}
+
+func (c *ConnMock) SetDeadline(t time.Time) error {
+	args := c.Called(t)
+	return args.Error(0)
+}
+
+func (c *ConnMock) SetReadDeadline(t time.Time) error {
+	args := c.Called(t)
+	return args.Error(0)
+}
+
+func (c *ConnMock) SetWriteDeadline(t time.Time) error {
+	args := c.Called(t)
+	return args.Error(0)
+}
 
 func Test_New_Client(t *testing.T) {
 	c.Convey("NewClient must return Client.", t, func() {
-		client := NewClient()
+		client := NewClient(&ConnMock{})
 
 		packet := roc.NewPacket(1, "Hello World")
 		ret := client.Packer.Pack(packet)
-		fmt.Println(ret)
 
 		c.So(len(ret), c.ShouldEqual, 19)
+	})
+}
+
+func Test_Send(t *testing.T) {
+	c.Convey("NewClient must return Client.", t, func() {
+		m := &ConnMock{}
+		m.On("Write", mock.Anything).Return(3, nil)
+		client := NewClient(m)
+
+		ret, _ := client.Send([]byte("sss"))
+
+		c.So(1, c.ShouldBeGreaterThanOrEqualTo, ret)
 	})
 }
